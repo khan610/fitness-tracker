5355f88c1d8c71a6f769858102b907f2d0dc8dd9
Andrea Trajkovic <trajkovicandrea@gmail.com>
2019-05-18 03:10:02 +0200

    Mat Sidenav and Mat Form

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+import { MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule } from '@angular/material';
 
 @NgModule({
   imports: [
     MatButtonModule, 
     MatIconModule, 
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatToolbarModule 
   ],
   exports: [
     MatButtonModule, 
     MatIconModule, 
     MatFormFieldModule, 
-    MatInputModule
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCheckboxModule,
+    MatSidenavModule ,
+    MatToolbarModule
   ]
 })
 export class MaterialModule {}
