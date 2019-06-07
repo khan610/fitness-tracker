163f933b9c976cf06964b20084a31fe7ddb2903f
Andrea Trajkovic <trajkovicandrea@gmail.com>
2019-06-07 23:10:20 +0200

    Adding an Auth Reducer (and Actions) part 1

--- src/app/app.reducer.ts
+++ src/app/app.reducer.ts
@@ -1,13 +1,16 @@
 import { ActionReducerMap, createFeatureSelector, createSelector } from '@ngrx/store';
 
 import * as fromUi from './shared/ui.reducer';
+import * as fromAuth from './auth/auth.reducer';
 
 export interface State {
   ui: fromUi.State;
+  auth: fromAuth.State;
 }
 
 export const reducers: ActionReducerMap<State> = {
-  ui: fromUi.uiReducer
+  ui: fromUi.uiReducer,
+  auth: fromAuth.AuthReducer
 };
 
 export const getUiState = createFeatureSelector<fromUi.State>('ui');
